4262f7b39bd183ee14b0d5e9e14f898b9f820086
ddamme <ddamme05@gmail.com>
2026-09-16 19:44:20 -0400

    Replace portfolio with relocation notice

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,9 @@
-import { BrowserRouter } from 'react-router-dom';
-import {
-  About,
-  Contact,
-  Experience,
-  Hero,
-  Navbar,
-  Tech,
-  Projects,
-} from './components';
-
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="relative z-0">
-        <div>
-          <Navbar />
-          <Hero />
-        </div>
-
-        <div className="bg-about bg-cover bg-center bg-no-repeat">
-          <About />
-        </div>
-
-        <div className="bg-tech bg-cover bg-center bg-no-repeat pb-10">
-          <Tech />
-        </div>
-
-        <Projects />
-
-        <div
-          className="bg-experience bg-cover bg-center bg-no-repeat 
-            rounded-tl-[150px] rounded-br-[150px]">
-          <div
-            className="bg-experienceLight bg-cover bg-center 
-            bg-no-repeat rounded-tl-[150px] rounded-br-[130px]">
-            <Experience />
-          </div>
-        </div>
-        <div className="relative z-0">
-          <Contact />
-        </div>
-      </div>
-    </BrowserRouter>
+    <main>
+      This site has been relocated to{' '}
+      <a href="https://dilans.dev">dilans.dev</a>.
+    </main>
   );
 };
 
